49510b1cff33e44329db2bb57d7848bb20320b0a
blaze <horvathb0222@gmail.com>
2024-03-21 21:41:37 +0100

    Refactor env

---
+++ src/Exceptions/ConfigNotInitializedException.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bring\BlocksWP\Exceptions;
+
+use Exception;
+use Throwable;
+
+// No direct access
+defined("ABSPATH") or die("Hey, do not do this 😱");
+
+class ConfigNotInitializedException extends Exception {
+	public function __construct(string $message = "", int $code = 0, Throwable $previous = null) {
+		parent::__construct($message, $code, $previous);
+	}
+}
